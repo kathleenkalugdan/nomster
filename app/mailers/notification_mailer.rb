521d0be57a6a03c7d38a3247d07e20bd16678b9d
kathleen kalugdan <kay16_alcantara@yahoo.com.ph>
2019-05-11 00:26:34 +0000

    adjust mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,11 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
-    mail(to: "kay16_alcantara@yahoo.com.ph",
-      subject: "A commemt has been added to your place")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+
+    mail(to: @place_owner.email,
+      subject: "A commemt has been added #{@place.name}")
   end
 end
